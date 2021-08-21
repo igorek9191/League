f62e92ae4c8fd4d7ec4bff4fda446e509484472a
Власюк Игорь Игоревич <IVlasyuk@alfabank.ru>
2021-08-22 00:21:51 +0300

    Forgot about @Component annotation

--- src/test/java/league/test/task/BaseTest.java
+++ src/test/java/league/test/task/BaseTest.java
@@ -1,15 +1,10 @@
 package league.test.task;
 
-import league.test.task.endpoints.BreedsEndpoint;
-import league.test.task.endpoints.CategoriesEndpoint;
-import league.test.task.endpoints.FavouritesEndpoint;
-import league.test.task.endpoints.ImagesEndpoint;
 import league.test.task.request_spec_handler.RequestSpecHandler;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(classes = MyApplication.class)
 public class BaseTest {
